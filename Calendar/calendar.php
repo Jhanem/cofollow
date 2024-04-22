5256fa50d291a65f7a215aea05908377ee21a00a
Jjustinj <167666696+Jjustinj@users.noreply.github.com>
2024-04-22 11:02:11 +0800

    Update calendar.php

--- Calendar/calendar.php
+++ Calendar/calendar.php
@@ -11,6 +11,24 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    $todo_date = $_POST['month'] . '-' . $_POST['day'] . '-' . $_POST['year'];
+    $todo_title = $_POST['set_title'];
+    $todo_description = $_POST['setdesc'];
+    
+
+    // Prepare SQL statement
+    $sql = "INSERT INTO todolists (todo_date, todo_title, todo_description) VALUES ('$todo_date', '$todo_title', '$todo_description')";
+
+    // Execute SQL statement
+    if ($conn->query($sql) === TRUE) {
+        echo "<script>alert ('New record inserted succesfully.'); window.location.href=' /Co-Follow/Calendar/index.php'; </script>";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
 $conn->close();
 ?>
